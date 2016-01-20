802f31c59858e2fd4a05fb76390438138af9fc58
SirLopez <daniel.lopez@live.se>
2016-01-20 10:57:57 +0100

    Started the labb

---
+++ ServiceLabb2/CountingService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLabb2
+{
+    [ServiceContract(Namespace = "Countingservice")]
+    public interface ICounting
+    {
+        [OperationContract]
+        double Bmi(double number);
+
+        [OperationContract]
+        DateTime DaysOld(DateTime days);
+
+        [OperationContract]
+        DateTime HoursOld(DateTime hours);
+        
+    }
+
+    public class CountingService : ICounting
+    {
+        public double Bmi(double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime DaysOld(DateTime days)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime HoursOld(DateTime hours)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WCFLabb2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLabb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service_Days");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(DaysService), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint
+                     (typeof(IDays),
+                     new WSHttpBinding(),
+                     "DaysService");
+
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+
+                    selfServiceHost.Open();
+                    Console.WriteLine("The gates are open!");
+                    Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                    Console.ReadLine();
+
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    selfServiceHost.Close();
+                }
+            }
+        }
+    }
+}
