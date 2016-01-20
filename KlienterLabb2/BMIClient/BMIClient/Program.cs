054fa040d359908bb17a313920019e2fd595da7f
SirLopez <daniel.lopez@live.se>
2016-01-20 13:26:47 +0100

    Done with BMI, both console and winform

--- KlienterLabb2/BMIClient/BMIClient/Program.cs
+++ KlienterLabb2/BMIClient/BMIClient/Program.cs
@@ -10,6 +10,19 @@ namespace BMIClient
     {
         static void Main(string[] args)
         {
+            CountingClient BMI = new CountingClient();
+            
+            //Nu ska jag ta in två nummer från användaren, först vikt sen längd.
+            //bla bla bla
+            Console.WriteLine("Welcome, let´s calculate your BMI \r\nPlease input your weight.");
+            var weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please insert your height in cm.");
+            var height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(BMI.Bmi(weight, height));
+            Console.ReadKey();
+
         }
     }
 }
