f9db4f1005308f5ffc80398501d72819b272594d
SirLopez <daniel.lopez@live.se>
2016-01-20 20:55:33 +0100

    Started last assignment on Labb2,

---
+++ WCFLabb2Hobby/ServiceHobby/DriverClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHobby
+{
+    [ServiceContract]
+    public interface ITeam
+    {
+        [OperationContract]
+        List<string> GetTeamNames();
+    }
+    public class TeamClass : ITeam
+    {
+
+        public List<string> GetTeamNames()
+        {
+            return new List<string>() {@"Mercedes", "Ferrari", "Williams", "Red Bull Racing", "Force India", "Lotus", "Torro Rosso", "Sauber", "McLaren", "Manor", "Haas"};
+        }
+    }
+}
---
+++ WCFLabb2Hobby/WCFLabb2Hobby/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceHobby;
+
+namespace WCFLabb2Hobby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/ServiceHobby");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(TeamClass), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint(
+                        typeof (ITeam),
+                        new WSHttpBinding(),
+                        "TeamClass");
+
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpsGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+
+                    selfServiceHost.Open();
+                    Console.WriteLine("Do you want to see a list of drivers?");
+                    Console.WriteLine("Press ENTER to exit");
+                    Console.ReadKey();
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    selfServiceHost.Close();
+                }
+            }
+        }
+    }
+}
