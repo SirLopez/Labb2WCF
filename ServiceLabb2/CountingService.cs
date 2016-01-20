e13b6685dc0ad6c409d69e76a8ba7e7f5254f946
SirLopez <daniel.lopez@live.se>
2016-01-20 11:15:13 +0100

    Added some base stuff for the project

--- ServiceLabb2/CountingService.cs
+++ ServiceLabb2/CountingService.cs
@@ -10,31 +10,38 @@ namespace ServiceLabb2
     [ServiceContract(Namespace = "Countingservice")]
     public interface ICounting
     {
+        //1. BMI
         [OperationContract]
-        double Bmi(double number);
-
+        double Bmi(double weight, double height);
+        //2. Years/Days/Hours
         [OperationContract]
-        DateTime DaysOld(DateTime days);
-
-        [OperationContract]
-        DateTime HoursOld(DateTime hours);
+        DateTime WhatIsMyAgeIn(DateTime days);
         
+        [OperationContract]
+        DateTime ThousandDaysEven(DateTime thousandDays);
+
     }
 
     public class CountingService : ICounting
     {
-        public double Bmi(double number)
+
+        public double Bmi(double weight, double height)
         {
+            //Calculate (weight / 2*(height))
+            //Return result
             throw new NotImplementedException();
         }
 
-        public DateTime DaysOld(DateTime days)
+        public DateTime WhatIsMyAgeIn(DateTime days)
         {
+            //Calculate years and days and hours using datetime
+            //Return result depending on what the user choose
             throw new NotImplementedException();
         }
 
-        public DateTime HoursOld(DateTime hours)
+        public DateTime ThousandDaysEven(DateTime thousandDays)
         {
+            //When is my next age an even thousand days?
             throw new NotImplementedException();
         }
     }
