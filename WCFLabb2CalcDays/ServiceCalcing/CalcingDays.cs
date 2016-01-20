18d509b9fdb2ba1253cabcf5f1eee2b7731318a6
SirLopez <daniel.lopez@live.se>
2016-01-20 13:44:45 +0100

    Starting with assignement 2, calculate days old, new solution and projects coming

---
+++ WCFLabb2CalcDays/ServiceCalcing/CalcingDays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ServiceCalcing
+{
+    [ServiceContract(Namespace = "Calculate days")]
+    public interface ICalcDaysOld
+    {
+        [OperationContract]
+        int daysOld(DateTime bornDate, DateTime today);
+    }
+    public class CalcingDays : ICalcDaysOld
+    {
+        public int daysOld(DateTime bornDate, DateTime today)
+        {
+            Console.WriteLine("Recieved");
+            return today.Day - bornDate.Day;
+        }
+    }
+}
---
+++ WCFLabb2CalcDays/WCFLabb2CalcDays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using ServiceCalcing;
+
+namespace WCFLabb2CalcDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/ServiceCalcing");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(CalcingDays), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint
+                     (typeof(ICalcDaysOld),
+                     new WSHttpBinding(),
+                     "CalcingDays");
+
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+
+                    selfServiceHost.Open();
+                    Console.WriteLine("Let´s calculate how many days you are");
+                    Console.WriteLine("Press ENTER to exit");
+                    Console.ReadLine();
+
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    selfServiceHost.Close();
+                }
+            }
+        }
+    }
+}
